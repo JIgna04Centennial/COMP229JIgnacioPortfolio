fdd5b6bb77c713bb87093fb8543fede4deba7e05
James Ignacio <jigna04@my.centennialcollege.ca>
2024-09-26 03:08:34 -0400

    Update vite.config.js

--- JIgnacioPortfolioApp-vite/vite.config.js
+++ JIgnacioPortfolioApp-vite/vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      // This section is used for excluding dependencies from the bundle, not local files.
-      external: [/src/components/Project], // You probably don't need this unless you have external dependencies to exclude.
+      // Removed the invalid external configuration
     }
   }
 })
