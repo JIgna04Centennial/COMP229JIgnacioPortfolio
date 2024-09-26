df42bc691169e26fe2ecbc694cf485c131d297ab
James Ignacio <jigna04@my.centennialcollege.ca>
2024-09-26 03:06:07 -0400

    Update vite.config.js

--- JIgnacioPortfolioApp-vite/vite.config.js
+++ JIgnacioPortfolioApp-vite/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // This section is used for excluding dependencies from the bundle, not local files.
+      external: [/src/components/Project], // You probably don't need this unless you have external dependencies to exclude.
+    }
+  }
 })
